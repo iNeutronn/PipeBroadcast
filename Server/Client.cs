465a131a1cd36a820331a8c1d97e88fd01371c9f
iNeutronn <57719073+iNeutronn@users.noreply.github.com>
2023-11-30 13:40:45 +0200

    Added initial architecture to server part

---
+++ Server/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Client
+    {
+        private NamedPipeServerStream _pipeServer;
+        private int _id;
+        private bool _IsSubscribedToWeater;
+        private bool _IsSubscribedToShares;
+        private bool _IsSubscribedToCurency;
+    }
+}
---
+++ Server/DataParsing/ICashedDataParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.DataParsing
+{
+    internal abstract class ICashedDataParser<T>
+    {
+        protected TimeSpan TimeOut;
+        protected DateTime LastUpdate;
+        protected T CachedData;
+        
+        public T GetData()
+        {
+            if (DateTime.Now - LastUpdate > TimeOut)
+            {
+                LastUpdate = DateTime.Now;
+                return GetDataFromSource();
+            }
+            else
+            {
+                return CachedData;
+            }
+        }
+
+        protected abstract T GetDataFromSource();
+       
+    }
+}
