d18a18f8d06425f5db0abbc4f427ecd96e143e66
bashivan-lpnu <ivan.bash.pz.2022@lpnu.ua>
2023-12-03 18:33:46 +0200

    some new functionality

--- Server/ClientManager.cs
+++ Server/ClientManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,14 @@ namespace Server
     {
         private List<Client> _clients;
         private int _idCounter;
+        private Task _idThread;
+
 
         public ClientManager()
         {
             _clients = new List<Client>();
             _idCounter = 0;
+            _idThread = Task.Run(() => idThreadWork());
         }
 
         public IEnumerator<Client> GetEnumerator()
@@ -27,6 +31,32 @@ namespace Server
         {
             return ((IEnumerable)_clients).GetEnumerator();
         }
-    }
 
+        private async Task idThreadWork()
+        {
+            using (NamedPipeServerStream idPipe = new NamedPipeServerStream("idPipe", PipeDirection.InOut, 10, PipeTransmissionMode.Byte, PipeOptions.Asynchronous))
+            {
+                while (true)
+                {
+                    await idPipe.WaitForConnectionAsync();
+
+                    Guid clientId = Guid.NewGuid();
+
+                    _clients.Add(new Client(clientId.ToString()));
+                    ++_idCounter;
+
+                    byte[] clientIdBytes = clientId.ToByteArray();
+
+                    idPipe.Write(clientIdBytes, 0, clientIdBytes.Length);
+
+                    byte[] confirmation = new byte[1];
+                    int bytesRead = await idPipe.ReadAsync(confirmation, 0, confirmation.Length);
+
+                    idPipe.Disconnect();
+                }
+            }
+        }
+
+
+    }
 }
