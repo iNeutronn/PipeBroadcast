b9645a464703a7aa25246637d484c7db119f2ec1
iNeutronn <57719073+iNeutronn@users.noreply.github.com>
2023-12-04 10:45:20 +0200

    Deleted unnesesary usings

--- Server/DataParsing/DataObjects/Weather/DailyForecast.cs
+++ Server/DataParsing/DataObjects/Weather/DailyForecast.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Server.DataParsing.DataObjects.Weather
 {
     public class DailyForecast
