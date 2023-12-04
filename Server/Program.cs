43bc3b612f14cd1b6597b723a2ffa1d705dbeecb
iNeutronn <57719073+iNeutronn@users.noreply.github.com>
2023-12-04 10:35:32 +0200

    Added Curency Parsing

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Server.DataParsing;
 
-WheatherTest();// no needed / can be changed to any other function
+CurencyTest();// no needed / can be changed to any other function
 
 
 void WheatherTest()
@@ -9,4 +9,10 @@ void WheatherTest()
     var data = parser.GetData();
     Console.WriteLine(data.Headline.Text);
 
+}
+void CurencyTest()
+{
+    CurencyDataParser parser = new CurencyDataParser(TimeSpan.FromMinutes(5));
+    var data = parser.GetData();
+    Console.WriteLine(data);
 }
